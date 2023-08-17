6a629947de2520ed39260360805cd40fb4de104d
Niraj Kumar Thakur <niraj.thakur@moonraft.com>
2023-08-17 16:32:26 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,33 @@
+{
+  /* <div id="parent">
+    <div id="child">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+    <div id="child1">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+</div> */
+}
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child", key: 1 }, [
+    React.createElement("h1", { key: 1 }, "I'm h1 tag"),
+    React.createElement("h2", { key: 2 }, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child1", key: 2 }, [
+    React.createElement("h1", { key: 3 }, "I'm h1 tag"),
+    React.createElement("h2", { key: 4 }, "I'm h2 tag"),
+  ]),
+]);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello world from React!"
+// );
+// console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(parent);

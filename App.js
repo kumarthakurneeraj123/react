9e22d2d5b8d2d4b8c7ac9fe830e4226952b20d0c
Niraj Kumar Thakur <niraj.thakur@moonraft.com>
2023-08-19 16:58:49 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,36 +1,56 @@
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id="child1">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div> */
-}
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: 1 }, [
-    React.createElement("h1", { key: 1 }, "I'm niraj"),
-    React.createElement("h2", { key: 2 }, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child1", key: 2 }, [
-    React.createElement("h1", { key: 3 }, "I'm h1 tag"),
-    React.createElement("h2", { key: 4 }, "I'm h2 tag"),
-  ]),
+import React from "react";
+import ReactDOM from "react-dom/client";
+import farmerLogo from "./resources/images/heart.png";
+import userIcon from "./resources/images/userIcon.png";
+// 1 task
+const heading = React.createElement("div", { class: "title" }, [
+  React.createElement("h1", {}, "React from H1 tag"),
+  React.createElement("h2", {}, "React from H2 tag"),
+  React.createElement("h3", {}, "React from H3 tag"),
 ]);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello world from React!"
-// );
-// console.log(parent);
+// 2nd task
+const headingJSX = (
+  <div className="title" tabIndex={1}>
+    <h1>React from JSX h1</h1>
+    <h2>React from JSX h2</h2>
+    <h3>React from JSX h3</h3>
+  </div>
+);
+
+// 3rd task
+const headingJSXComponent = () => (
+  <div className="title" tabIndex={1}>
+    <Title />
+    <h1>React from JSX h1</h1>
+    <h2>React from JSX h2</h2>
+    <h3>React from JSX h3</h3>
+  </div>
+);
+
+function Title() {
+  return (
+    <div>
+      <h1>Title</h1>
+    </div>
+  );
+}
+
+const HeaderComponent = () => {
+  return (
+    <div className="container">
+      <div>
+        <img className="img" alt="logo" src={farmerLogo} />
+      </div>
+      <div className="search">
+        <input type="search" />
+      </div>
+      <div id="user-icon">
+        <img className="img" src={userIcon} alt="user-icon" />
+      </div>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(parent);
+root.render(<HeaderComponent />);
